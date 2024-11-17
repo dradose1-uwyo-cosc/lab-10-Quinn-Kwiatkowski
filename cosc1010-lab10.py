744ba8423d088dd3cbf470570979767297eed423
Quinn-Kwiatkowski <qkwiatko@uwyo.edu>
2024-11-17 00:01:40 +0000

    I haven't saved it in a minute

--- cosc1010-lab10.py
+++ cosc1010-lab10.py
@@ -11,10 +11,54 @@ from hashlib import sha256
 from pathlib import Path
 
 def get_hash(to_hash):
-    """You can use """
+    """You can use"""
+    # ^ YIPIEEE
+    # Converts string to 'utf-8'. Hashes with 'sha256' returns the final value in uppercase
     return sha256(to_hash.encode('utf-8')).hexdigest().upper()
 
+def crack_password(hash_file_path, passord_list_path):
+    try:
+        # Open the 'hash' file in read mode
+        with open(hash_file_path, 'r') as hash_file:
+            # Read the contents and strip any whitespace around the password
+            target_hash = hash_file.read().strip() 
+    except FileNotFoundError:
+        # Missing file error
+        print(f'Error: hash file was not found, wompity womp')
+        return # Exits the program
+    except IOError:
+        # Permissions error
+        print(f'Error: unable to read that john')
+        return # Exits the program
+    
+    try:
+        # Open the 'rockyou.txt.' file in read mode, ignoring characers that it can not decode
+        with open(passord_list_path, 'r', encoding='utf-8', errors='ignore') as password_file:
+            # Iterates over each line in the file
+            for line in password_file:
+                # Strip any white space in the password
+                password = line.strip()
+                # Compares hash of the current password with the target hasg
+                if get_hash(password) == target_hash:
+                    # If found print the matching password
+                    print(f'Password found: {password}')
+                    return # Exits the program
+    except FileNotFoundError:
+        # Missing file error
+        print(f'Error: hash file was not found, wompity womp')
+        return # Exits the program
+    except IOError:
+        # Permissions error
+        print(f'Error: unable to read that john')
+        return # Exits the program
+    else: 
+        print("Password not found in the forbes 100 list.")
+    
+# Runing the function
+hash_file_path = 'hash' # Specifies specific path to the 'hash'
+password_list_path = 'rockyou.txt' # Specifies specific path the 'rockyou.txt'
 
+crack_password(hash_file_path, password_list_path) 
 
 # Files and Exceptions
 
